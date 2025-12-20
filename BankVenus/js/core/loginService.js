d750e976176c4737c601fdd3bafbb543cad6d027
BwunLevain <vivienne114bengtsson@gmail.com>
2025-12-20 21:50:27 +0100

    refactored logIn function

--- BankVenus/js/core/loginService.js
+++ BankVenus/js/core/loginService.js
@@ -1,8 +1,15 @@
 function logIn(bank, username, password){
-    for(let i = 0; i < bank.users.length; i++){
-        if(username == bank.users[i].username || password == bank.users[i].password){
-            return bank.users[i].id;
-        }
+    // for(let i = 0; i < bank.users.length; i++){
+    //     if(username == bank.users[i].username || password == bank.users[i].password){
+    //         return bank.users[i].id;
+    //     }
+    // }
+    // return 0;
+
+    let user = bank.users.find(user => user.username === username);
+    if(user.password === password){
+        return user;
+    }else {
+        return 0;
     }
-    return 0;
 }
